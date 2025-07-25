abed124782987284532aa5946a08b3719416e0db
Iván Ortega García <iaog_250998@hotmail.com>
2025-07-24 20:12:42 -0600

    Se agrego el diagrama de fluo y el diagrma entidad relación

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ivan_og/StudentOverFlow/.venv
+set -gx VIRTUAL_ENV /home/iv-n-alberto-ortega-garc-a/StudentOverFlow/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
